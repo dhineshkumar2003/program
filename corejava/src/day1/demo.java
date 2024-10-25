600272d38a1e9266e3551725eacebd216bf56d0a
dhineshkumar2003 <dhineshtn0@gmail.com>
2024-10-25 11:08:35 +0530

    commit one

---
+++ corejava/src/day1/demo.java
@@ -0,0 +1,17 @@
+package day1;
+import java.util.*;
+public class demo {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner s= new Scanner (System.in);
+		System.out.println("enter the a values:");
+		a=s.nextInt();
+		System.out.println("enter the b values:");
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Addition of a="+a+"and b="+b);
+		System.out.println("c="+c);	
+	}
+
+}
